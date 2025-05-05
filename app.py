14263381e29d83602d3fc18760993fbd3ac9c916
Hosam.Dighidy <55159449+hdighidy@users.noreply.github.com>
2025-05-05 19:38:15 +0300

    app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with tab2:
 
     st.subheader("📈 Evaluation After Cleaning")
     st.write(f"R2 Score: {r2_score(y_test2, preds):.3f}")
-    st.write(f"RMSE: {mean_squared_error(y_test2, preds, squared=False):.3f}")
+    st.write(f"RMSE: {mean_squared_error(y_test2, preds):.3f}")
     model_results[method] = (r2_score(y_test2, preds), mean_squared_error(y_test2, preds, squared=False))
 
     # Create subplots
